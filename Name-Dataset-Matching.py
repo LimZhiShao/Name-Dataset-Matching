5b0de32bd7314496161ff5c4ee8a76dec630b682
LimZhiShao <88354498+LimZhiShao@users.noreply.github.com>
2021-08-03 12:14:05 +0800

    Update Name-Dataset-Matching.py

--- Name-Dataset-Matching.py
+++ Name-Dataset-Matching.py
@@ -1,3 +1,21 @@
+/*
+ *Modifications Copyright 2017, ING Wholesale Banking Advanced Analytics Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+
 import pandas as pd
 
 memberlist=pd.read_excel(r'C:\Users\user\Desktop\Combined member list.xlsx',header=None,usecols=[0,2])  #in original dataset, column 0 is member's name, column 2 is the year of the member's registration
